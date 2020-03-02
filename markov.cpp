05eb89d20aed57bb201c82418d79ca91f635588b
Biggkahuna <sederquest.alex@gmail.com>
2020-03-02 12:36:07 -0500

    working

--- markov.cpp
+++ markov.cpp
@@ -11,13 +11,20 @@ void MarkovGenerator::InsertWord(std::string* word) {
         // Check for the first word being added to the chain
         //mMarkovChain.at(*word) = new std::vector<Node>();
         if (this->mPrevWord != "") { // Not the first word
-            for (auto itr = mMarkovChain[*word].begin(); itr != mMarkovChain[*word].end(); itr++) {
-                if (itr->getWord())
-            }
             Node node;
-            node.setWord(&mPrevWord);
+            node.setWord(word);
             node.increment();
-            mMarkovChain[*word].push_back(node);
+            mMarkovChain[this->mPrevWord].push_back(node);
+            // for (auto itr = mMarkovChain[this->mPrevWord].begin(); itr != mMarkovChain[this->mPrevWord].end(); itr++) {
+            //     if (itr->getWord() == *word) {
+            //         itr->increment();
+            //     } else {
+            //         Node node;
+            //         node.setWord(word);
+            //         node.increment();
+            //         mMarkovChain[this->mPrevWord].push_back(node);
+            //     }
+            // }
         }
     }
 }
